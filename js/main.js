87bdbd5ab95cd02561437d009df8965896394c5c
Przemek <przemyslawjakubas24@gmail.com>
2023-10-26 14:51:22 +0200

    Add animation in nav links, scroll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 const burgerBtn = document.querySelector('.burger')
 const navList = document.querySelector('.nav-main__list')
+const sizeNav = document.querySelector('.nav-main__container')
+const sizeNavLogo = document.querySelector('.logo')
+const sizeNavFont = document.querySelectorAll('.nav-main__item')
 
 burgerBtn.addEventListener('click', () => {
 	burgerBtn.classList.toggle('burger--active')
 	navList.classList.toggle('nav-main__list--active')
 })
+
+function handleScroll() {
+	if (window.scrollY >= 200) {
+		sizeNav.style.padding = '1em'
+		sizeNavLogo.style.width = '40px'
+		sizeNavFont.forEach(element => {
+			element.style.fontSize = '1.4rem'
+		})
+	} else {
+		sizeNav.style.padding = '2em'
+		sizeNavLogo.style.width = '50px'
+		sizeNavFont.forEach(element => {
+			element.style.fontSize = '1.6rem'
+		})
+	}
+}
+
+window.addEventListener('scroll', handleScroll)
